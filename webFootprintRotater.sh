6bba428b9c325e8782e4ac53575c85bbd3ff1a7f
TopChainBoy <topchainboy@thedaoma.com>
2024-03-06 00:38:41 +0100

    Refactored script for better readability and maintainability. Introduced functions to check if a command or directory exists. Improved directory existence checks for Mozilla and Chrome directories using these functions. Also, used variables to store directory paths for better code reuse.

--- webFootprintRotater.sh
+++ webFootprintRotater.sh
@@ -5,8 +5,18 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "For preventing canvas fingerprinting, you can use browser extensions like CanvasBlocker."
 
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" &> /dev/null
+}
+
+# Function to check if a directory exists
+directory_exists() {
+    [ -d "$1" ]
+}
+
 # Check if ProtonVPN is installed
-if command -v protonvpn &> /dev/null; then
+if command_exists protonvpn; then
     # Switch IP
     protonvpn c -r
 else
@@ -14,23 +24,25 @@ else
 fi
 
 # Check if Mozilla directory exists
-if [ -d ~/.mozilla/firefox/*.default ]; then
+MOZILLA_DIR=$(find ~/.mozilla/firefox/*.default -maxdepth 0 -type d 2>/dev/null)
+if directory_exists "$MOZILLA_DIR"; then
     # Clear cookies
-    sqlite3 ~/.mozilla/firefox/*.default/cookies.sqlite "DELETE FROM moz_cookies"
+    sqlite3 "$MOZILLA_DIR/cookies.sqlite" "DELETE FROM moz_cookies"
 
     # Change user agent
-    echo 'user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");' >> ~/.mozilla/firefox/*.default/prefs.js
+    echo 'user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");' >> "$MOZILLA_DIR/prefs.js"
 else
     echo "Mozilla ~/.mozilla/firefox/ directory not found."
 fi
 
 # Check if Chrome directory exists
-if [ -d ~/.config/google-chrome/Default ]; then
+CHROME_DIR=~/.config/google-chrome/Default
+if directory_exists "$CHROME_DIR"; then
     # Clear cookies
-    rm ~/.config/google-chrome/Default/Cookies
+    rm "$CHROME_DIR/Cookies"
 
     # Change user agent
-    echo 'chrome://flags/#user-agent' >> ~/.config/google-chrome/Default/Preferences
+    echo 'chrome://flags/#user-agent' >> "$CHROME_DIR/Preferences"
 else
     echo "Chrome ~/.config/google-chrome/ directory not found."
 fi
